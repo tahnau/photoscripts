f2e78cfcaafdaf9c152719c62957457f8c6dd803
tahnau <1890770+tahnau@users.noreply.github.com>
2025-06-08 13:13:40 +0300

    Update phone_move_images.sh

--- phone_move_images.sh
+++ phone_move_images.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define source and destination directories
-SOURCE_DIR="/media/my_images/ios_photobackup_folder/"
-DEST_DIR="/media/my_images/000_INPUT"
+# ./phone_move_images.sh /media/my_images/ios_photobackup_folder/ /media/my_images/000_INPUT
 
-# Find and copy files modified in the last 2 months
-# find $SOURCE_DIR -type f -mtime -60 -exec cp {} $DEST_DIR \;
-find "$SOURCE_DIR" -type f -mtime -60 -not -name '.photobackup' -exec mv {} $DEST_DIR \;
+# Usage check
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 SOURCE_DIR DEST_DIR [DAYS]"
+  exit 1
+fi
 
+# Args (default to 60 days)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-60}
+
+# Move files modified in the last $DAYS days (excluding .photobackup)
+find "$SOURCE_DIR" -type f -mtime -"$DAYS" \
+     ! -name '.photobackup' \
+     -exec mv -t "$DEST_DIR" {} +
+
+# Fix ownership & permissions on the source tree
+chown -R :halkousers "$SOURCE_DIR"
+chmod -R g+rw       "$SOURCE_DIR"
+chmod    g+s        "$SOURCE_DIR"
